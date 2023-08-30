ee85cdb6168c4565b1b87ca70ed51eeec4cf251c
lokesh2518 <lokesh.tiwari@gravitonweb.com>
2023-08-30 15:59:35 +0530

    final

--- src/Modules/Components/Contact.jsx
+++ src/Modules/Components/Contact.jsx
@@ -1,17 +1,27 @@
-import React, { useState } from 'react';
-import { Col, Container, Form, Row} from "react-bootstrap";
-import {Link} from 'react-router-dom';
-import './Contact.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope , faPhone ,faLocationDot} from '@fortawesome/free-solid-svg-icons';
-import { faTwitter, faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import React, { useState } from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Contact.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faPhone,
+  faLocationDot,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faTwitter,
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Contact() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [textarea, setTextArea] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [textarea, setTextArea] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log({
@@ -23,112 +33,209 @@ export default function Contact() {
     });
 
     // Reset form fields
-    setFirstName('');
-    setLastName('');
-    setEmail('');
-    setPassword('');
-    setTextArea('');
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword("");
+    setTextArea("");
   };
-    
+
   return (
     <>
-    <div className='contact-bg-color'>
-    <Container>
-      <Row className='p-3'>
-        <Col md={6}>
-          <h1 className='contact-heading'>Contact Us</h1>
-        </Col>
-        <Col md={6}>
-          <div className='d-flex justify-content-end align-items-center h-100'>
-            <Link to="/" className="contact-link"><span className='contact-subheading'>Home</span></Link>
-            <span className='contact-subheading'>&nbsp;&gt;&nbsp;</span>
-            <span className='contact-subheading'>Contact Us</span>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-    </div>
-
-    <div className="contact-content-bgcolor">
-    <Container>
-    <Row className="pt-5 pb-5 contact-container-box">
-        <Col md={6}>
-        <h2 className="main-contact-heading">Let's Talk</h2>
-        <Form onSubmit={handleSubmit} className='pt-4'>
-        <Row>
-           <Col md={6}>
-           <div className="contact-input-field mb-3">
-            <input type="text" className="contact-input" placeholder="First Name"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)} ></input>
-            </div>
-           </Col>
-
+      <div className="contact-bg-color">
+        <Container>
+          <Row className="p-3">
             <Col md={6}>
-            <div className="contact-input-field mb-3">
-            <input type="text" className="contact-input" placeholder="Last Name"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}></input>
-            </div>
+              <h1 className="contact-heading">Contact Us</h1>
+            </Col>
+            <Col md={6}>
+              <div className="d-flex justify-content-end align-items-center h-100">
+                <Link to="/" className="contact-link">
+                  <span className="contact-subheading">Home</span>
+                </Link>
+                <span className="contact-subheading">&nbsp;&gt;&nbsp;</span>
+                <span className="contact-subheading">Contact Us</span>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
+      <div className="contact-content-bgcolor">
+        <Container>
+          <Row className="pt-5 pb-5 contact-container-box">
+            <Col md={6}>
+              <h2 className="main-contact-heading">Let's Talk</h2>
+              <Form onSubmit={handleSubmit} className="pt-4">
+                <Row>
+                  <Col md={6}>
+                    <div className="contact-input-field mb-3">
+                      <input
+                        type="text"
+                        className="contact-input"
+                        placeholder="First Name"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                      ></input>
+                    </div>
+                  </Col>
+
+                  <Col md={6}>
+                    <div className="contact-input-field mb-3">
+                      <input
+                        type="text"
+                        className="contact-input"
+                        placeholder="Last Name"
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                      ></input>
+                    </div>
+                  </Col>
+
+                  <Col md={6} className="mt-3">
+                    <div className="contact-input-field mb-3">
+                      <input
+                        type="email"
+                        className="contact-input"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      ></input>
+                    </div>
+                  </Col>
+
+                  <Col md={6} className="mt-3">
+                    <div className="contact-input-field mb-3">
+                      <input
+                        type="password"
+                        className="contact-input"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      ></input>
+                    </div>
+                  </Col>
+
+                  <Col md={12}>
+                    <textarea
+                      className="contact-input mt-3"
+                      rows="5"
+                      placeholder="Leave a comment here"
+                      value={textarea}
+                      onChange={(e) => setTextArea(e.target.value)}
+                    ></textarea>
+                  </Col>
+
+                  <div class="mt-4 text-center">
+                    <button
+                      type="submit"
+                      className="btn contact-button-hover px-5"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </Row>
+              </Form>
             </Col>
 
-            <Col md={6} className="mt-3">
-             <div className="contact-input-field mb-3">
-             <input type="email" className="contact-input" placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}></input>
-             </div>
+            <Col md={6} className="pt-4 contact-icons-details">
+              <h3 className="text-white">Contact Details</h3>
+
+              <ul class="list-inline mt-4">
+                <li>
+                  <Link href="#" className="contact-link">
+                    <FontAwesomeIcon
+                      icon={faEnvelope}
+                      className="contact1-icon"
+                    />
+                    ace.chronicle.official.id@gmail.com
+                  </Link>
+                </li>
+
+                <li className=" mt-2">
+                  <Link href="#" className="contact-link">
+                    <FontAwesomeIcon icon={faPhone} className="contact1-icon" />
+                    (+91) 7972-585-033
+                  </Link>
+                </li>
+
+                <li className=" mt-2">
+                  <Link href="#" className="contact-link">
+                    <FontAwesomeIcon
+                      icon={faLocationDot}
+                      className="contact1-icon"
+                    />
+                    Mumbai , Maharashtra
+                  </Link>
+                </li>
+
+                <li className=" mt-2">
+                  <Link href="#" className="contact-link">
+                    <FontAwesomeIcon
+                      icon={faWhatsapp}
+                      className="contact1-icon"
+                    />
+                    7972585033 , 9309317893
+                  </Link>
+                </li>
+
+                <li className=" mt-3 contact-link">
+                  Working Days : Monday - Saturday
+                </li>
+
+                <li className=" mt-2 contact-link">
+                  Working Hours : 10 AM - 9 PM IST
+                </li>
+
+                <li className=" mt-2 contact-link">
+                  Email & Whatsapp 24/7 available.
+                </li>
+              </ul>
+
+              <hr className="text-white mt-4"></hr>
+
+              <div className="mt-4">
+                <Link to="#">
+                  <FontAwesomeIcon
+                    icon={faFacebook}
+                    className="contact2-icon"
+                  />
+                </Link>
+
+                <Link to="#">
+                  <FontAwesomeIcon
+                    icon={faInstagram}
+                    className="contact2-icon"
+                  />
+                </Link>
+
+                <Link to="#">
+                  <FontAwesomeIcon icon={faTwitter} className="contact2-icon" />
+                </Link>
+
+                <Link to="#">
+                  <FontAwesomeIcon
+                    icon={faLinkedin}
+                    className="contact2-icon"
+                  />
+                </Link>
+              </div>
             </Col>
-
-            <Col md={6} className="mt-3">
-            <div className="contact-input-field mb-3">
-             <input type="password" className="contact-input" placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}></input>
-            </div>
-            </Col>
-
-            <Col md={12}>
-            <textarea
-            className="contact-input mt-3"
-            rows="5"
-            placeholder="Leave a comment here"
-            value={textarea}
-            onChange={(e) => setTextArea(e.target.value)}>
-           </textarea>
-            </Col>
-
-            <div class="mt-4 text-center">
-                <button type="submit" className="btn contact-button-hover px-5">Submit</button>
-            </div>
-        </Row>
-        </Form>
-        </Col>
-
-        <Col md={6} className='pt-5 contact-icons-details'>
-        <h3 className='text-white'>Contact Details</h3>
-        <ul class="list-inline mt-4">
-          <li><Link href="#" className='contact-link'><FontAwesomeIcon icon={faEnvelope} className="contact1-icon"/>
-          info@gravitonweb.com</Link></li>
-          <li className=" mt-3"><Link href="#" className='contact-link'><FontAwesomeIcon icon={faPhone} className="contact1-icon"/>
-          +91-8400952502</Link></li>
-          <li className=" mt-3"><Link href="#" className='contact-link'><FontAwesomeIcon icon={faLocationDot} className="contact1-icon"/>
-          Dubai office:Office No.904,Aspin
-          <br></br>Commercial Tower,Sheikh Zayed Road,<br></br>Dubai,P.O. Box 118658 UAE</Link></li>
+          </Row>
+        </Container>
+        <div
+          className="map-container"
+          style={{ padding: "20px", width: "calc(100% )" }}
+        >
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d241317.14571337597!2d72.71637411447857!3d19.082177513371683!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c6306644edc1%3A0x5da4ed8f8d648c69!2sMumbai%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1693390573268!5m2!1sen!2sin"
+            frameBorder="0"
+            style={{ border: 0, width: "100%", height: "calc(100vh - 300px)" }}
+            allowFullScreen
+          ></iframe>
          
-        </ul>
-        <hr className='text-white mt-4'></hr>
-        <div className='mt-4'>
-        <Link to="#"><FontAwesomeIcon icon={faFacebook} className="contact2-icon"/></Link>
-        <Link to="#"><FontAwesomeIcon icon={faInstagram} className="contact2-icon"/></Link>
-        <Link to="#"><FontAwesomeIcon icon={faTwitter} className="contact2-icon"/></Link>
-        <Link to="#"><FontAwesomeIcon icon={faLinkedin} className="contact2-icon"/></Link>
         </div>
-        </Col>
-    </Row>
-    </Container>
-    </div>
-   
+      </div>
     </>
   );
 }
--- src/Modules/Components/Services.jsx
+++ src/Modules/Components/Services.jsx
@@ -1,11 +1,167 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Services.css";
+import { Col, Container, Row } from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
+import CardImages from "./Service images/Content-Writing.jpg";
+import CardImages1 from "./Service images/Designing.jpg";
+import CardImages2 from "./Service images/SMO.jpg";
+import CardImages3 from "./Service images/DIGITAL-MARKETING.jpg";
+import CardImages4 from "./Service images/ContentStrategy.jpg";
+import CardImages5 from "./Service images/CUSTOMISED-SERVICES.jpg";
 
 const Services = () => {
-  return (
-   <>
-   <h3>Services</h3>
-   </>
-  )
-}
+  const [isHovered, setIsHovered] = useState(false);
 
-export default Services
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <>
+      <div className='service-bg-color'>
+    <Container>
+      <Row className='p-3'>
+        <Col md={6}>
+          <h1 className='service-heading'>Services</h1>
+        </Col>
+        <Col md={6}>
+          <div className='d-flex justify-content-end align-items-center h-100'>
+            <Link to="/" className="service-link"><span className='service-subheading'>Home</span></Link>
+            <span className='service-subheading'>&nbsp;&gt;&nbsp;</span>
+            <span className='service-subheading'>Services</span>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+
+    </div>
+      <div className="some-cards2 ">
+    
+
+        <CardGroup className="our-cardgroup1">
+          <Card
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className="custom-card2"
+          >
+            <Card.Img className="some-img2" variant="top" src={CardImages} />
+
+            <Card.Body>
+              <div className="service-overlay1">
+                <div className="service1">
+                  <Card.Title>
+                    {isHovered ? "Content Writing" : "Content Writing"}
+                    <p className="service-para">Ace Chronicle</p>
+                  </Card.Title>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
+          <Card
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className="custom-card2"
+          >
+            <Card.Img className="some-img2" variant="top" src={CardImages1} />
+            <Card.Body>
+              <div className="service-overlay1">
+                <div className="service1">
+                  <Card.Title>
+                    {" "}
+                    {isHovered ? "Designing" : "Designing"}
+                    <p className="service-para">Ace Chronicle</p>
+                  </Card.Title>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
+          <Card
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className="custom-card2"
+          >
+            <Card.Img className="some-img2" variant="top" src={CardImages2} />
+            <Card.Body>
+              <div className="service-overlay1">
+                <div className="service1">
+                  <Card.Title>
+                    {" "}
+                    {isHovered
+                      ? "Social Media Management"
+                      : "Social Media Management"}
+                    <p className="service-para">Ace Chronicle</p>
+                  </Card.Title>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
+        </CardGroup>
+        <CardGroup className="our-cardgroup2">
+          <Card
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className="custom-card2"
+          >
+            <Card.Img className="some-img2" variant="top" src={CardImages3} />
+            <Card.Body>
+              <div className="service-overlay1">
+                <div className="service1">
+                  {/* <Card.Title>Digital Marketing</Card.Title> */}
+                  <Card.Title>
+                    {" "}
+                    {isHovered ? "Digital Marketing" : "Digital Marketing"}
+                    <p className="service-para">Ace Chronicle</p>
+                  </Card.Title>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
+          <Card
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className="custom-card2"
+          >
+            <Card.Img className="some-img2" variant="top" src={CardImages4} />
+            <Card.Body>
+              <div className="service-overlay1">
+                <div className="service1">
+                  <Card.Title>
+                    {" "}
+                    {isHovered ? "Strategy" : "Strategy"}
+                    <p className="service-para">Ace Chronicle</p>
+                  </Card.Title>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
+          <Card
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className="custom-card2"
+          >
+            <Card.Img className="some-img2" variant="top" src={CardImages5} />
+            <Card.Body>
+              <div className="service-overlay1">
+                <div className="service1">
+                  <Card.Title>
+                    {" "}
+                    {isHovered ? "Customised Service" : "Customised Service"}
+                    <p className="service-para">Ace Chronicle</p>
+                  </Card.Title>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
+        </CardGroup>
+      </div>
+    </>
+  );
+};
+
+export default Services;
